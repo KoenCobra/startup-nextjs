37686b768afe6ad4a4402e082ba7e2f825cd4907
Koen De Groot <koendegroot84@gmail.com>
2024-11-09 20:29:17 +0100

    added first card setup

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 import React from "react";
 
 const Home = async ({
@@ -9,6 +10,19 @@ const Home = async ({
   }>;
 }) => {
   const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _id: 1,
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "Unitree" },
+      description: "description",
+      category: "Robots",
+      image: "https://www.unitree.com/g1/",
+      title: "We Robots",
+    },
+  ];
   return (
     <>
       <section className='pink_container'>
@@ -22,7 +36,21 @@ const Home = async ({
         <SearchForm query={query} />
       </section>
 
-      <section></section>
+      <section className='section_container'>
+        <p className='text-30-semibold'>
+          {query ? `Serach results for "${query}"` : "All startups"}
+        </p>
+
+        <ul className='mt-7 card_grid'>
+          {posts.length > 0 ? (
+            posts.map((post: StartupCardType) => (
+              <StartupCard key={post._id} post={post} />
+            ))
+          ) : (
+            <p className='no-results'>No startups found</p>
+          )}
+        </ul>
+      </section>
     </>
   );
 };
