4ce2814ef9eb85be892e59febdbb4d9429a43cef
Koen De Groot <koendegroot84@gmail.com>
2024-11-09 19:56:36 +0100

    added search form

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,7 +1,30 @@
+import SearchForm from "@/components/SearchForm";
 import React from "react";
 
-const Home = () => {
-  return <h1>Home</h1>;
+const Home = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{
+    query: string;
+  }>;
+}) => {
+  const query = (await searchParams).query;
+  return (
+    <>
+      <section className='pink_container'>
+        <h1 className='heading'>
+          Pitch you startup, <br />
+          Connect with entrepreneurs
+        </h1>
+        <p className='sub-heading !max-w-3xl'>
+          Submit ideas, Vote on Pitches and get noticed in Virtual Competitions
+        </p>
+        <SearchForm query={query} />
+      </section>
+
+      <section></section>
+    </>
+  );
 };
 
 export default Home;
